b602ffe71ac8454085f4106486350533b66f57d2
Adrian Cucoș <dielorg@gmail.com>
2024-08-21 08:30:08 +0200

    make setup_2.py actually useful

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
+import logging
 import subprocess
 import sys
 import os
+
 
 def try_run(cmd: str):
     try:
@@ -9,19 +11,24 @@ def try_run(cmd: str):
     except subprocess.CalledProcessError as e:
         print(f"cmd failed: {e}\n{cmd}")
 
+
 def try_run_sudo(cmd: str):
+    logging.debug(f"trying to run command with sudo: {cmd}")
     try_run(f"sudo {cmd}")
 
+
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
+    print("You pressed Ctrl+C!")
     sys.exit(0)
+
 
 def open_terminal(command: str):
     os.system(f"gnome-terminal -- {command} &")
 
+
 def strip_uuid_and_id(path: str):
     path = path.lower()
-    words =  path.split(":")
+    words = path.split(":")
 
     if len(words) > 1:
         return tuple(words)
