ee8f07a2702026dfea89c8a4a8bb205a16c5fa0e
Cerenia <christelle.gloor@inf.ethz.ch>
2024-11-10 18:26:32 +0100

    fetched some useful shell methods from previous utils

--- setup/shell.py
+++ setup/shell.py
@@ -1,6 +1,8 @@
 import inspect
 import logging
 import shutil
+import sys
+import os
 
 run_command_counter = 0
 
@@ -60,3 +62,35 @@ def get_term():
         if shutil.which(terminal):
             return terminal
     return "gnome-terminal"
+
+
+def signal_handler(sig, frame):
+    print("You pressed Ctrl+C!")
+    sys.exit(0)
+
+
+def open_terminal(command: str):
+    os.system(f"gnome-terminal -- {command} &")
+
+
+class ColorHandler(logging.StreamHandler):
+    # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    GRAY8 = "38;5;8"
+    GRAY7 = "38;5;7"
+    ORANGE = "33"
+    RED = "31"
+    WHITE = "0"
+
+    def emit(self, record):
+        # Don't use white for any logging, to help distinguish from user print statements
+        level_color_map = {
+            logging.DEBUG: self.GRAY8,
+            logging.INFO: self.GRAY7,
+            logging.WARNING: self.ORANGE,
+            logging.ERROR: self.RED,
+        }
+
+        csi = f"{chr(27)}["  # control sequence introducer
+        color = level_color_map.get(record.levelno, self.WHITE)
+
+        print(f"{csi}{color}m{record.msg}{csi}m")
