2a48111a1c5c8f5ffa81caf707db137669e6b54e
Cerenia <christelle.gloor@inf.ethz.ch>
2024-11-26 13:45:54 +0100

    removed any files that were replaced by the new setup, the database, old tests and updated the gitignore

--- src/constants.py
+++
@@ -1,7 +0,0 @@
-from signal_protocol.curve import PublicKey
-# Extracted from the clients (so far no certificate revocation has happened on PROD, but staging might be more flaky)
-TRUST_ROOT_STAGING = b"BbqY1DzohE4NUZoVF+L18oUPrK3kILllLEJh2UnPSsEx"
-TRUST_ROOT_PROD = b"BXu6QIKVz5MA8gstzfOgRQGqyLqOwNKHL6INkv3IHWMF"
-
-TRUST_ROOT_STAGING_PK = PublicKey.from_base64(TRUST_ROOT_STAGING)
-TRUST_ROOT_PROD_PK = PublicKey.from_base64(TRUST_ROOT_PROD)
